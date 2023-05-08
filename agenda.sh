2da3200e726305e8610bd4f5195a604ce4cba01b
Brooks Townsend <brooks@cosmonic.com>
2023-05-08 11:54:26 -0400

    added workflow for workflow-dispatch create agenda
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- agenda.sh
+++ agenda.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -e
 # Nice little helper script to run the few commands
 
-[ -z "$1" ] && echo "did not supply MM-DD for script" && exit 1
+[ -z "$1" ] && echo "did not supply YYYY-MM-DD for script" && exit 1
 
 echo "Creating new agenda for $1"
 git checkout main
 git fetch
 git pull
-git checkout -b mtg/$1-meeting-agenda
 
-DST_FILE=community/2023-$1-community-meeting.md
-cp community/template.txt $DST_FILE
-# TODO: sed
-echo "Done! Edit $DST_FILE"
+DST_FILE=community/$1-community-meeting.md
+cat community/template.txt | sed "s/YYYY-MM-DD/$1/" > $DST_FILE
+echo "Done! Add agenda items, demos, and awesome stuff to $DST_FILE"
