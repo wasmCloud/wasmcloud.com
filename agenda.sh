9c244dae1949cb4436aea8381064fbc9c837da46
Brooks Townsend <brooks@cosmonic.com>
2023-07-19 14:10:01 -0400

    modified community action to support livestream url
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- agenda.sh
+++ agenda.sh
@@ -4,10 +4,11 @@
 [ -z "$1" ] && echo "did not supply YYYY-MM-DD for script" && exit 1
 
 echo "Creating new agenda for $1"
+echo "Livestream link $2"
 git checkout main
 git fetch
 git pull
 
 DST_FILE=community/$1-community-meeting.md
-cat community/template.txt | sed "s/YYYY-MM-DD/$1/" > $DST_FILE
+cat community/template.txt | sed "s/YYYY-MM-DD/$1/" | sed "s/VIDEOURL/$2/" > $DST_FILE
 echo "Done! Add agenda items, demos, and awesome stuff to $DST_FILE"
