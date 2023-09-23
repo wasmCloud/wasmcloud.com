130e59fb9ea12cacea9022d581c1f0bbdf5af5d4
Yordis Prieto <yordis.prieto@gmail.com>
2023-09-23 02:30:45 -0400

    chore: improve a11y
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Declarative WebAssembly App Orchestration',
-    Image: '/img/features/cloud.png',
+    image: {src: '/img/features/cloud.png', alt: 'Multiple type of devices around wasmCloud logo'},
     description: (
       <>
         Write your application as a <b>declarative</b> set of WebAssembly components. Deploy your application to any cloud, edge, or IoT device with a single command.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Edge-to-Cloud Compute Mesh',
-    Image: '/img/features/connected.png',
+    image: {src: '/img/features/connected.png', alt: 'A message bus connecting multiple wasmCloud hosts'},
     description: (
       <>
         Using <b>NATS</b>, wasmCloud hosts cluster together across disparate clouds and infrastructure, distributing your apps with a single flat topology without ever opening a single firewall port. Clusters self-form and self-heal to automatically enable load balancing and failover without building it into your app.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Vendorless WebAssembly Components',
-    Image: '/img/features/secure.png',
+    image: {src: '/img/features/secure.png', alt: 'The wasmCloud logo inside a lock'},
     description: (
       <>
         wasmCloud WebAssembly components are completely <b>vendorless</b> and don't tie you to any platform, cloud, or proprietary implementation. Swap out implementations and change your underlying infrastructure at any time without changing your application code.
@@ -32,14 +32,14 @@ const FeatureList = [
   },
 ];
 
-function Feature({ Image, title, description }) {
+function Feature({ image, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img src={Image} />
+        <img {...image} />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+        <h2>{title}</h2>
         <p>{description}</p>
       </div>
     </div>
