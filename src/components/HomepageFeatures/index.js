c8eb64e6809768287013a439b00b978f5ea4ff3c
Brooks Townsend <brooksmtownsend@gmail.com>
2023-12-13 10:07:54 -0500

    feat: updated homepage values
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,8 +4,8 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Declarative WebAssembly App Orchestration',
-    image: {src: '/img/features/cloud.png', alt: 'Multiple type of devices around wasmCloud logo'},
+    title: 'Declarative WebAssembly Orchestration',
+    image: { src: '/img/features/cloud.png', alt: 'Multiple type of devices around wasmCloud logo' },
     description: (
       <>
         Write your application as a <b>declarative</b> set of WebAssembly components. Deploy your application to any cloud, edge, or IoT device with a single command.
@@ -13,8 +13,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Edge-to-Cloud Compute Mesh',
-    image: {src: '/img/features/connected.png', alt: 'A message bus connecting multiple wasmCloud hosts'},
+    title: 'Seamless Distributed Networking',
+    image: { src: '/img/features/connected.png', alt: 'A message bus connecting multiple wasmCloud hosts' },
     description: (
       <>
         Using <b>NATS</b>, wasmCloud hosts cluster together across disparate clouds and infrastructure, distributing your apps with a single flat topology without ever opening a single firewall port. Clusters self-form and self-heal to automatically enable load balancing and failover without building it into your app.
@@ -22,8 +22,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Vendorless WebAssembly Components',
-    image: {src: '/img/features/secure.png', alt: 'The wasmCloud logo inside a lock'},
+    title: 'Vendorless Application Components',
+    image: { src: '/img/features/secure.png', alt: 'The wasmCloud logo inside a lock' },
     description: (
       <>
         wasmCloud WebAssembly components are completely <b>vendorless</b> and don't tie you to any platform, cloud, or proprietary implementation. Swap out implementations and change your underlying infrastructure at any time without changing your application code.
