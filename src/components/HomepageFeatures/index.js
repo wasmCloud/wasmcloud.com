e0e1df3b47a7f22592603c6a2fb1a4337aba5526
boomskats <nik@boemskats.com>
2024-01-26 22:30:58 +0000

    style: features section new layout and dark mode image switch
    Signed-off-by: boomskats <nik@boemskats.com>
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import { useColorMode } from '@docusaurus/theme-common';
 
+const root = '/img/features';
 const FeatureList = [
   {
     title: 'Declarative WebAssembly Orchestration',
-    image: { src: '/img/features/cloud.png', alt: 'Multiple type of devices around wasmCloud logo' },
+    image: { name: 'cloud', alt: 'Multiple type of devices around wasmCloud logo' },
+    imageFirst: true,
     description: (
       <>
         Write your application as a <b>declarative</b> set of WebAssembly components. Deploy your application to any cloud, edge, or IoT device with a single command.
@@ -14,7 +17,8 @@ const FeatureList = [
   },
   {
     title: 'Seamless Distributed Networking',
-    image: { src: '/img/features/connected.png', alt: 'A message bus connecting multiple wasmCloud hosts' },
+    image: { name: 'connected', alt: 'A message bus connecting multiple wasmCloud hosts' },
+    imageFirst: false,
     description: (
       <>
         Using <b>NATS</b>, wasmCloud hosts cluster together across disparate clouds and infrastructure, distributing your apps with a single flat topology without ever opening a single firewall port. Clusters self-form and self-heal to automatically enable load balancing and failover without building it into your app.
@@ -23,7 +27,8 @@ const FeatureList = [
   },
   {
     title: 'Vendorless Application Components',
-    image: { src: '/img/features/secure.png', alt: 'The wasmCloud logo inside a lock' },
+    image: { name: 'secure', alt: 'The wasmCloud logo inside a lock' },
+    imageFirst: true,
     description: (
       <>
         wasmCloud WebAssembly components are completely <b>vendorless</b> and don't tie you to any platform, cloud, or proprietary implementation. Swap out implementations and change your underlying infrastructure at any time without changing your application code.
@@ -32,30 +37,53 @@ const FeatureList = [
   },
 ];
 
-function Feature({ image, title, description }) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img {...image} />
-      </div>
-      <div className="text--center padding-horiz--md">
+function FeatureImageComponent({ image }) {
+    const { colorMode } = useColorMode();
+    return (
+        <img className={styles.featureSvg} src={root 
+            + '/feature-' 
+            + image.name 
+            + (colorMode === 'dark' ? "-dark.svg" : "-light.svg") } 
+            alt={image.alt} 
+        />
+    );
+}
+
+function FeatureTextComponent({ title, description }) {
+    return (
+      <div className={styles.featureText} align="left">
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
+    );
+}
+
+function Feature({ image, title, description, imageFirst }) {
+    if (imageFirst == true) {
+        return (
+            <div className={styles.featureRow}>
+                  <FeatureImageComponent image={image} />
+                  <FeatureTextComponent title={title} description={description} />
+            </div>
+        ) 
+    }
+
+    return (
+    <div className={styles.featureRow}>
+          <FeatureTextComponent title={title} description={description} />
+          <FeatureImageComponent image={image} />
     </div>
-  );
+    );
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+    <div className={styles.features}>
+        <div className={styles.featuresContainer}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
-      </div>
-    </section>
+    </div>
   );
 }
