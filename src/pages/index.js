2736e97d3805fb94b9e4164cd9a828e50ee90542
boomskats <nik@boemskats.com>
2024-01-31 17:09:22 +0000

    chore: prettifying the unpretty
    Signed-off-by: boomskats <nik@boemskats.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import SecurityAudit from "@site/src/components/SecurityAudit";
-import { useColorMode } from "@docusaurus/theme-common";
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import SecurityAudit from '@site/src/components/SecurityAudit';
+import { useColorMode } from '@docusaurus/theme-common';
 
-import styles from "./index.module.css";
-import NewsSection from "../components/NewsSection";
+import styles from './index.module.css';
+import NewsSection from '../components/NewsSection';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -21,9 +21,7 @@ function HomepageHeader() {
             <img
               id="cloudspic"
               src="img/dancing-clouds.svg"
-              class={colorMode === "dark"
-                ? styles.cloudsDark
-                : styles.cloudsLight}
+              class={colorMode === 'dark' ? styles.cloudsDark : styles.cloudsLight}
               alt="logo"
             />
           </div>
@@ -34,31 +32,24 @@ function HomepageHeader() {
               <br />
               {siteConfig.customFields.tagline_2}
             </p>
-            {
-              /* <div>
+            {/* <div>
           Build your functions and services in the language you want and run them securely everywhere with WebAssembly.
-        </div> */
-            }
+        </div> */}
             <div className={styles.heroButton}>
-              <Link
-                className={clsx("button", "button--lg", styles.button)}
-                to="/docs/installation"
-              >
+              <Link className={clsx('button', 'button--lg', styles.button)} to="/docs/installation">
                 Try it yourself
               </Link>
             </div>
           </div>
         </div>
       </div>
-      {
-        /* <div className={styles.terminal} id="terminal">
+      {/* <div className={styles.terminal} id="terminal">
         wash new
         <br />
         wash app deploy
         <br />
         wash up
-      </div> */
-      }
+      </div> */}
     </header>
   );
 }
