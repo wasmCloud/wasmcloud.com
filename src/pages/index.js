4a19105565b46748fd79966b1541e916350aea53
boomskats <nik@boemskats.com>
2024-01-31 12:35:14 +0000

    style: responsive, css cleanup
    Signed-off-by: boomskats <nik@boemskats.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,63 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import SecurityAudit from '@site/src/components/SecurityAudit';
-import { useColorMode } from '@docusaurus/theme-common';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import SecurityAudit from "@site/src/components/SecurityAudit";
+import { useColorMode } from "@docusaurus/theme-common";
 
-import styles from './index.module.css';
-import NewsSection from '../components/NewsSection';
+import styles from "./index.module.css";
+import NewsSection from "../components/NewsSection";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   return (
     <header className={clsx(styles.hero, styles.heroBanner)}>
-      <div className="container" >
-        <div className={clsx('row', styles.heroItems)}>
-          <div className='col' align='right'>
-            <img id="cloudspic" 
-                src={colorMode === 'dark' ? "img/heroclouds-dark.svg" : "img/heroclouds-light.svg"}
-                alt="logo" className={styles.heroLogo} />
+      <div className="container">
+        <div className={styles.heroItems}>
+          <div className={styles.heroGraphic}>
+            <img
+              id="cloudspic"
+              src={colorMode === "dark"
+                ? "img/heroclouds-dark.svg"
+                : "img/heroclouds-light.svg"}
+              alt="logo"
+            />
           </div>
-          <div className='col'>
-            <div className={styles.heroText} align='left'>
-                <h1>{siteConfig.title}</h1>
-                <p> {siteConfig.customFields.tagline_1}
-                <br />
-                {siteConfig.customFields.tagline_2}
-                </p>
-        {/* <div>
+          <div className={styles.heroText}>
+            <h1>{siteConfig.title}</h1>
+            <p>
+              {siteConfig.customFields.tagline_1}
+              <br />
+              {siteConfig.customFields.tagline_2}
+            </p>
+            {
+              /* <div>
           Build your functions and services in the language you want and run them securely everywhere with WebAssembly.
-        </div> */}
-        <div className={styles.heroButton}>
-          <Link
-            className="button button--frontpage button--lg"
-            to="/docs/installation">
-            Try it yourself
-          </Link>
-        </div>
+        </div> */
+            }
+            <div className={styles.heroButton}>
+              <Link
+                className={clsx("button", "button--lg", styles.button)}
+                to="/docs/installation"
+              >
+                Try it yourself
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-        </div>
-      </div>
-      {/* <div className={styles.terminal} id="terminal">
+      {
+        /* <div className={styles.terminal} id="terminal">
         wash new
         <br />
         wash app deploy
         <br />
         wash up
-      </div> */}
+      </div> */
+      }
     </header>
   );
 }
@@ -57,10 +65,11 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout 
+    <Layout
       wrapperClassName="homepage"
       title={`Homepage`}
-      description="wasmCloud - Why stop at the Edge?">
+      description="wasmCloud - Why stop at the Edge?"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
