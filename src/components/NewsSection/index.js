4d70ab22567eeb962479e00d6155b05659274c87
boomskats <nik@boemskats.com>
2024-01-31 17:09:22 +0000

    chore: prettifying the unpretty
    Signed-off-by: boomskats <nik@boemskats.com>
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/components/NewsSection/index.js
+++ src/components/NewsSection/index.js
@@ -1,16 +1,15 @@
-import React from "react";
-import styles from "./styles.module.css";
-import { useColorMode } from "@docusaurus/theme-common";
-import clsx from "clsx";
+import React from 'react';
+import styles from './styles.module.css';
+import { useColorMode } from '@docusaurus/theme-common';
+import clsx from 'clsx';
 
 function NewsImageComponent() {
   const { colorMode } = useColorMode();
-  const root = "/img/";
+  const root = '/img/';
   return (
     <img
       className={styles.newsSvg}
-      src={root +
-        (colorMode === "dark" ? "news-dark.svg" : "news-light.svg")}
+      src={root + (colorMode === 'dark' ? 'news-dark.svg' : 'news-light.svg')}
       alt="News Image"
     />
   );
@@ -27,14 +26,13 @@ export default function NewsSection() {
             <h1>wasmCloud Security Assessment</h1>
             <div>
               <i>
-                "WasmCloud is a well reviewed project, with lots of diligence in
-                its security posture. This has paid off, as evidenced by this
-                audit, which had{" "}
+                "WasmCloud is a well reviewed project, with lots of diligence in its security
+                posture. This has paid off, as evidenced by this audit, which had{' '}
                 <strong>no severe or high issues to resolve</strong>."
               </i>
             </div>
             <a
-              className={clsx("button", "button--lg", styles.button)}
+              className={clsx('button', 'button--lg', styles.button)}
               href="https://ostif.org/ostif-has-completed-a-security-audit-of-wasmcloud/"
             >
               Read more on OSTIF.org
