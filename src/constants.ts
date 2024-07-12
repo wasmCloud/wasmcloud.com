1c74d1dc8d1ba5d6644f8aa64b67ec93aa40ac03
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-07-12 16:32:26 -0400

    feat(blog): use dedicated blog components to avoid upgrade issues
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+const Links = {
+  YOUTUBE: 'https://www.youtube.com/watch?v=Q6J9Q6Q6Q6Q',
+  PLAYLIST: 'https://www.youtube.com/@wasmCloud/videos?view=2&sort=dd&live_view=503&shelf_id=3',
+  CALENDAR:
+    'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
+  ZOOM: 'https://us02web.zoom.us/j/89608468294?pwd=Z0Z2Z0J2b0J2b0J2b0J2b0J2b0J2Zz09',
+  GITHUB: 'https://github.com/wasmcloud/wasmcloud',
+  SLACK: 'https://slack.wasmcloud.com',
+} as const;
+
+export { Links };
