fc87d03841a94230a3790067b31b89e4d7dcc601
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-07-25 12:35:35 -0400

    fix(pages/home): finalize font decision
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '@theme-original/Layout';
 import type LayoutType from '@theme/Layout';
 import type { WrapperProps } from '@docusaurus/types';
-import { FontPicker } from '@site/src/pages/_components/font-picker';
 
 type Props = WrapperProps<typeof LayoutType>;
 
@@ -10,7 +9,6 @@ export default function LayoutWrapper(props: Props): JSX.Element {
   return (
     <>
       <Layout {...props} />
-      <FontPicker />
     </>
   );
 }
