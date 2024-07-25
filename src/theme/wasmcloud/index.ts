ecfac4102a053f8f07686c256d4cdf1167301379
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-07-25 17:39:58 -0400

    fix(pages/home): add use cases section to the homepage
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

---
+++ src/theme/wasmcloud/index.ts
@@ -0,0 +1,15 @@
+declare module '@theme/wasmcloud/components/video-modal' {
+  export * from '@site/src/theme/wasmcloud/components/video-modal';
+}
+
+declare module '@theme/wasmcloud/hooks/use-body-scroll-lock' {
+  export * from '@site/src/theme/wasmcloud/hooks/use-body-scroll-lock';
+}
+
+declare module '@theme/wasmcloud/hooks/use-click-outside' {
+  export * from '@site/src/theme/wasmcloud/hooks/use-click-outside';
+}
+
+declare module '@theme/wasmcloud/hooks/use-keypress' {
+  export * from '@site/src/theme/wasmcloud/hooks/use-keypress';
+}
