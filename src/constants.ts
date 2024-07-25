c44d833d3693745a7ab277e3efc07e4bb482f46b
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-07-25 13:39:25 -0400

    fix(pages/home): update youtube link handling
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 const Links = {
-  YOUTUBE: 'https://www.youtube.com/watch?v=Q6J9Q6Q6Q6Q',
-  PLAYLIST: 'https://www.youtube.com/@wasmCloud/videos?view=2&sort=dd&live_view=503&shelf_id=3',
+  YOUTUBE: 'https://www.youtube.com/watch?v=7_273whOiF8',
+  PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
   ZOOM: 'https://us02web.zoom.us/j/89608468294?pwd=Z0Z2Z0J2b0J2b0J2b0J2b0J2b0J2Zz09',
