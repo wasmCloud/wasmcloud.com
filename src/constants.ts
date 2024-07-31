e903727e24e887f7255e9b8d60706ddda46c8427
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-07-31 13:18:03 -0400

    fix: correct zoom link
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ const Links = {
   PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
-  ZOOM: 'https://us02web.zoom.us/j/89608468294?pwd=Z0Z2Z0J2b0J2b0J2b0J2b0J2b0J2Zz09',
+  ZOOM: 'https://zoom.us/j/98144613430?pwd=VjBObmZIaUppUXVWRkxSZ3crZzhkZz09',
   GITHUB: 'https://github.com/wasmcloud/wasmcloud',
   SLACK: 'https://slack.wasmcloud.com',
 } as const;
