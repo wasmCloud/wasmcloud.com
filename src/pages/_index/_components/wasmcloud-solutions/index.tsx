8f538fb1f2adeb2613fe650a7c2c9fa7514fb490
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-08-06 14:10:46 -0400

    feat(pages/home): update stakes section text and add icons
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/pages/_index/_components/wasmcloud-solutions/index.tsx
+++ src/pages/_index/_components/wasmcloud-solutions/index.tsx
@@ -3,9 +3,11 @@ import { Section, SectionColor } from '@site/src/pages/_components/section';
 import { SectionHeading } from '@site/src/pages/_components/section-heading';
 import { SectionTag } from '@site/src/pages/_components/section-tag';
 import styles from './styles.module.css';
-import { SectionLayout } from '@site/src/pages/_components/section-layout';
 import { Grid, GridItem } from '@site/src/pages/_components/grid';
 import { SectionContent } from '@site/src/pages/_components/section-content';
+import SvgCost from '@site/static/pages/home/icon/cost.svg';
+import SvgUpdates from '@site/static/pages/home/icon/updates.svg';
+import SvgDistributed from '@site/static/pages/home/icon/distributed.svg';
 
 type Props = {
   color?: SectionColor;
@@ -15,30 +17,35 @@ function WasmCloudSolutions({ color = 'light-gray' }: Props) {
   return (
     <Section color={color} id="wasmcloud-solutions">
       <SectionContent align="center">
-        <SectionTag>Why use wasmCloud?</SectionTag>
+        <SectionTag>wasmCloud Solutions</SectionTag>
         <SectionHeading>Are you a team struggling with...</SectionHeading>
       </SectionContent>
       <SectionContent>
         <Grid className={styles.content} center>
           <GridItem>
-            <Placeholder size="50px" />
+            <SvgCost style={{ color: 'var(--section-color-highlight)' }} />
             <h4>Idle Infrastructure?</h4>
-            <p>Does scalability planning & long cold starts leave you with 
-              inefficient Infrastructure?
+            <p>
+              Does scalability preparedness, unpredictable workloads and long cold starts leave you
+              with inefficient infrastructure and ever-growing cloud costs?
             </p>
           </GridItem>
           <GridItem>
-            <Placeholder size="50px" />
+            <SvgUpdates style={{ color: 'var(--section-color-highlight)' }} />
             <h4>Maintaining Apps?</h4>
-            <p>Are developers patching the same 
-              vulnerabilities across 100's or 1,000's of apps?
+            <p>
+              Are developers patching the same vulnerabilities and managing updates and dependencies
+              across 100's or 1,000's of apps? Do you have apps that fall behind your golden
+              template?
             </p>
           </GridItem>
           <GridItem>
-            <Placeholder size="50px" />
-            <h4>Cross Clouds?</h4>
-            <p>Do you need to run your apps close to your users? Multi-Region? On the Edge? On Prem? 
-              Disconnected?</p>
+            <SvgDistributed style={{ color: 'var(--section-color-highlight)' }} />
+            <h4>Distributed Deployments?</h4>
+            <p>
+              Do you need to run your apps across cloud vendors? Close to your users? Multi-Region?
+              On the Edge? On Prem? Disrupted, disconnected, intermittent or limited connectivity?
+            </p>
           </GridItem>
         </Grid>
       </SectionContent>
