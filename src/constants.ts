411a7225913ab87477c3fff965f4570ca09ce8c0
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-09-18 11:19:28 -0400

    change to inno day link
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ const Links = {
   PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
-  ZOOM: 'https://zoom.us/j/98144613430?pwd=VjBObmZIaUppUXVWRkxSZ3crZzhkZz09',
+  ZOOM: 'https://cosmonic.zoom.us/w/82196859951',
   GITHUB: 'https://github.com/wasmcloud/wasmcloud',
   SLACK: 'https://slack.wasmcloud.com',
 } as const;
