08adb302785b6c14484e85d66dfac678068fe209
CazJT <CazJT@users.noreply.github.com>
2024-12-06 13:58:30 +0000

    added community meeting agenda for 2024-12-11
    Signed-off-by: GitHub <noreply@github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const Links = {
-  YOUTUBE: 'https://www.youtube.com/watch?v=F3U6ZqFCrB8',
+  YOUTUBE: 'https://www.youtube.com/watch?v=oZeaX11XoS4',
   PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
