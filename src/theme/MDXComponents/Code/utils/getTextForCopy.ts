d0c3ec716f7f16498cabbab81cc305ed7f9b82a4
markkovari <kovarimarkofficial@gmail.com>
2025-07-24 17:46:52 +0200

    fix: react children docusaurus rendered elements are copiable blocks
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- src/theme/MDXComponents/Code/utils/getTextForCopy.ts
+++ src/theme/MDXComponents/Code/utils/getTextForCopy.ts
@@ -4,12 +4,12 @@ function nodeIsReactElement<P extends Record<string, unknown>>(
   node: React.ReactNode,
   props?: P,
 ): node is React.ReactElement<P> {
+  // Most of the times the copiable elements are within one span and as children finger
+  if (React.Children.count(node) === 1 && React.isValidElement(node) && node.type === "span") {
+    return true;
+  }
   if (!React.isValidElement(node)) return false;
-
-  const hasAllProps = props && Object.keys(props).every((key) => key in (node.props as object));
-  if (!hasAllProps) return false;
-
-  return true;
+  return props && Object.keys(props).every((key) => key in (node.props as object));
 }
 
 function hasClassName(node: React.ReactNode, className: string): boolean {
@@ -27,39 +27,47 @@ function hasReactChildren(
 
 function getTextForCopy(node?: React.ReactNode | null): string {
   if (node === null || node === undefined) return '';
+  return stripDiffSpacer(getTextFromNode(node));
+}
 
+// the `\uFEFF` character is the ZERO WIDTH NO-BREAK SPACE that is invisible in most fonts
+const WHITESPACE_REPLACEMENT_STRING = '\uFEFF \n' as const;
+
+// should be identical to `WHITESPACE_REPLACEMENT_STRING` but with the additional newline char
+const WHITESPACE_REPLACEMENT_REGEXP = /\uFEFF \n\n/g;
+const stripDiffSpacer = (str: string): string => str.replace(WHITESPACE_REPLACEMENT_REGEXP, '');
+
+const getTextFromNode = (node: React.ReactNode): string => {
   // When getting all the text from a node, we want to remove the diff lines that were marked as
   // "removed" in the diff. Since code blocks are split up by line, and here we are only handling
   // one line at a time, we can't remove the following newline at the same time. To do that, we are
   // adding a recognizable but invisible whitespace character which will be stripped later.
 
-  // the `\uFEFF` character is the ZERO WIDTH NO-BREAK SPACE that is invisible in most fonts
-  const WHITESPACE_REPLACEMENT_STRING = '\uFEFF \n';
-
-  // should be identical to `WHITESPACE_REPLACEMENT_STRING` but with the additional newline char
-  const WHITESPACE_REPLACEMENT_REGEXP = /\uFEFF \n\n/g;
-  const stripDiffSpacer = (str: string): string => str.replace(WHITESPACE_REPLACEMENT_REGEXP, '');
-
-  const getTextFromNode = (node: React.ReactNode): string => {
-    switch (typeof node) {
-      case 'string':
-      case 'number':
-        return node.toString();
-      case 'boolean':
-        return '';
-      case 'object':
-        if (node instanceof Array) return React.Children.map(node, getTextForCopy).join('');
-        if (hasReactChildren(node)) return getTextFromNode(node.props.children);
-        if (!nodeIsReactElement(node)) return '';
-        // This is where we handle the case of a diff line that is marked as "removed".
-        if (hasClassName(node, 'diff remove')) return WHITESPACE_REPLACEMENT_STRING;
-        return '';
-      default:
-        return '';
-    }
-  };
-
-  return stripDiffSpacer(getTextFromNode(node));
-}
+  switch (typeof node) {
+    case 'string':
+    case 'number':
+      return node.toString();
+    case 'boolean':
+      return '';
+    case 'object':
+      // The code snippet declares that this part is historical, removed with this step
+      if (hasClassName(node, 'diff remove')) return WHITESPACE_REPLACEMENT_STRING;
+      // Recursion since the multiple level of the copiable contens react segmentaion
+      if (hasReactChildren(node)) {
+        return getTextFromNode(node.props.children)
+      };
+      // Recursion since the multiple level of the copiable contens react segmentaion
+      if (node instanceof Array) {
+        return React.Children.map(node, getTextForCopy).join('');
+      }
+      // Cannot serialise to make it copiable
+      if (!nodeIsReactElement(node)) return '';
+      // Default empty
+      return '';
+    default:
+      // Fallback default empty
+      return '';
+  }
+};
 
 export { getTextForCopy };
