920dcbebfafbfe1bd7da0ac94fcf6f6b1f49b155
brooksmtownsend <12040685+brooksmtownsend@users.noreply.github.com>
2025-08-27 16:14:06 +0000

    added community meeting agenda for 2025-08-27
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const Links = {
-  YOUTUBE: 'https://www.youtube.com/watch?v=rZSZ87g_82M',
+  YOUTUBE: 'https://www.youtube.com/watch?v=PnBGuEKab0o',
   PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
