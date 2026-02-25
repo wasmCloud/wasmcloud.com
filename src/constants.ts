51ba8246572abb21c5e8a12defdea8ae6aeaf09e
LiamRandall <1530656+LiamRandall@users.noreply.github.com>
2026-02-25 16:43:47 +0000

    added community meeting agenda for 2026-02-25
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const Links = {
-  YOUTUBE: 'https://youtube.com/live/-8FoV7pnWgg',
+  YOUTUBE: 'https://www.youtube.com/watch?v=WfZCHymKZ-Q',
   PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
