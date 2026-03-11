12d6ccf17164f0cc25844b28ad92bc0fc3a0144f
LiamRandall <1530656+LiamRandall@users.noreply.github.com>
2026-03-11 16:10:24 +0000

    added community meeting agenda for 2026-03-11
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: Bailey Hayes <bailey@cosmonic.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const Links = {
-  YOUTUBE: 'https://www.youtube.com/watch?v=WfZCHymKZ-Q',
+  YOUTUBE: 'https://youtube.com/live/IwAeQxahqgA?feature=share',
   PLAYLIST: 'https://www.youtube.com/@wasmCloud/streams',
   CALENDAR:
     'https://calendar.google.com/calendar/r/eventedit/copy/MnA2a2R1MnI2aGxucDZwNjUxMmVuMW1lb2FfMjAyNDA3MTBUMTcwMDAwWiBjXzZjbTVodWQ4ZXZ1bnM0cGU1Z2d1M2g5cXJzQGc',
